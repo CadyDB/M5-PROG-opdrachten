dc7a7ea069ba124d21a2a6c8f99f721818a10156
CadyDB <40341@ma-web.nl>
2026-09-10 12:53:55 +0200

    it works :3

---
+++ Assets/Scripts/PROG_Les1.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+ class PROG_Les1 : MonoBehaviour
+{
+    void Start()
+    {
+        string Naam = "Cady";
+        int Score = 1000;
+        bool Alive = true;
+        
+        Debug.Log("Naam: " + Naam);
+        Debug.Log("Score: " + Score);
+        Debug.Log("Alive: " + Alive);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # M5-PROG-opdrachten
 
 ## Les 1
-dit gedeelte wordt voor Les 1 van M5.
+Dit gedeelte wordt voor Les 1 van M5.
+
+
+
+## Les 2
+Dit gedeelte wordt voor Les 2 van M5.
